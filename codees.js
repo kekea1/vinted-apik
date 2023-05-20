e75c7c76d5260e6f7f6b5f4b7105e00e8fa69611
kekea1 <53305663+kekea1@users.noreply.github.com>
2023-05-20 13:32:06 +0200

    Update codees.js

--- codees.js
+++ codees.js
@@ -1,6 +1,6 @@
 
-import search from "./index.js";
-search('https://www.vinted.fr/vetements?brand_id[]=53&page=3').then((posts) => {
+vinted=require('./index.js')
+vinted.search('https://www.vinted.fr/vetements?brand_id[]=53&page=3').then((posts) => {
     console.log(posts);
     alert(posts[0]) // all the posts that match this query
 });
